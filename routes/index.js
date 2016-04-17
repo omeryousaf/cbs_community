659b148922e56f32301fa29bf3ea789a1f52c695
Omer Yousaf <u.yusaf@gmail.com>
2016-04-17 23:41:02 +0500

    promisified some calls and removed some logs

--- routes/index.js
+++ routes/index.js
@@ -64,7 +64,6 @@ router.getMember = function ( req, res ) {
 };
 
 router.getProfileImage = function ( req, res ) {
-    console.log('\ngetProfileImage req.user', req.user, '\n');
     profileEditor.getProfileImage(req.query.docid, req.query.picname).pipe(res);
 };
 
--- services/profile_edit.js
+++ services/profile_edit.js
@@ -3,7 +3,10 @@
  */
 var fs = require('fs');
 var config = require('../nodejs_config/config.js');
-var base64 = require('node-base64-image');
+var Promise = require( 'bluebird');
+var readFile = Promise.promisify(fs.readFile);
+var writeFile = Promise.promisify(fs.writeFile);
+var unlink = Promise.promisify(fs.unlink);
 
 module.exports = function ( nano ) { // var nano is passed in from caller routes/index
     var profileUpdator = {};
@@ -19,51 +22,37 @@ module.exports = function ( nano ) { // var nano is passed in from caller routes
         /* TASKS:
         * 1 - can we make all async calls using promises here ?
         * */
-        fs.writeFile( imagePath, base64Data, 'base64', function(err) {
-            if( err) {
-                console.log('\n', err, '\n');
-                res.send({ 'error': err });
-                return;
-            }
-            fs.readFile( imagePath, function (err, file) {
-                if ( !err ) {
-                    membersDb.get( memberId, { revs_info: true }, function(err, doc) {
-                        if ( err ) {
-                            console.log('\n', err, '\n');
-                            res.send({ 'error': err });
-                            return;
-                        } else {
-                            console.log('uploading image to couch server...');
-                            doc.currentImage = imageName;
-                            membersDb.multipart.insert( doc, [{name: imageName, data: file, content_type: 'image'}], doc._id, function(err, body) {
-                                if ( !err ) {
-                                    console.log('image uploaded!!!');
-                                    var imageCouchPath = config.App.serverIp + '/profileimage?docid=' + doc._id + '&picname=' + imageName;
-                                    res.send({ 'serverResponse': 'image uploaded!!!', 'filePath': imageCouchPath });
-                                    fs.unlink( imagePath, function (error) {
-                                        if ( error) {
-                                            return console.log( '\n', error.stack, '\n');
-                                        } else {
-                                            return console.log( '\nsuccess deleted file' + imageName + ' after saving to couch!\n');
-                                        }
-                                    });
-                                } else {
-                                    console.log('\n', err, '\n');
-                                    res.send({ 'error': err });
-                                }
+        return writeFile( imagePath, base64Data, 'base64').then( function () {
+            return readFile( imagePath);
+        }).then( function ( file) {
+            membersDb.get( memberId, { revs_info: true }, function(err, doc) {
+                if ( err ) {
+                    throw new Error( 'services::profile_edit::updatePicture::error fetching document');
+                } else {
+                    doc.currentImage = imageName;
+                    membersDb.multipart.insert( doc, [{name: imageName, data: file, content_type: 'image'}], doc._id, function(err, body) {
+                        if ( !err ) {
+                            var imageCouchPath = config.App.serverIp + '/profileimage?docid=' + doc._id + '&picname=' + imageName;
+                            res.send({
+                                serverResponse: 'image uploaded!!!',
+                                filePath: imageCouchPath
                             });
+                            return unlink( imagePath);
+                        } else {
+                            throw new Error( 'services::profile_edit::updatePicture::error uploading attachment');
                         }
                     });
-                } else {
-                    console.log('\n', err, '\n');
-                    res.send({ 'error': err });
                 }
+            });
+        }).catch( function ( error) {
+            console.log('\n', error.stack, '\n');
+            res.send({
+                error: err.stack
             });
         });
     };
 
     profileUpdator.getMember = function ( req, res ) {
-        console.log('fetching user profile...');
         console.log('\nreq.user: ', req.user, '\n');
         membersDb.get( req.params.id, {revs_info: true}, function (err, doc) {
             if (!err) {
