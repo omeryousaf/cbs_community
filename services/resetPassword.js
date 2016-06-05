5a2d4e93d011a262aa400749956df954989cc001
Shujaat Ali <m.ali.hussan.hussain@gmail.com>
2016-06-05 12:22:16 -0500

    successfully decoded the jwt token at another file and got user id from it

--- services/resetPassword.js
+++ services/resetPassword.js
@@ -1,8 +1,11 @@
 /**
  * Created by shujaatali on 05/06/16.
  */
+var config = require('../nodejs_config/config.js');
+var jwt = require('jsonwebtoken');
 module.exports.tryResettingPass = function(req,res){
-
+    var decode = jwt.verify(req,'CBS_Community');
+    console.log(decode);
 
 };
 
