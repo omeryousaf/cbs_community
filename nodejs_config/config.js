ea57d0eb3a2c603c032cc415e08eb15413ec0a9c
Shujaat Ali <m.ali.hussan.hussain@gmail.com>
2016-08-28 00:05:13 +0500

    added the secret key into the config

--- nodejs_config/config.js
+++ nodejs_config/config.js
@@ -5,5 +5,6 @@ exports.App = {
     CouchServerIp: 'http://127.0.0.1:5984',
     serverIp: 'http://localhost:3000',
     mailGunApiKey: 'key-9a6d6bd3a96a3315693cdc72dc651a43',
-    mailGunDomain : 'sandboxe91368ca64a2490d9d308fd1d4df5971.mailgun.org'
+    mailGunDomain : 'sandboxe91368ca64a2490d9d308fd1d4df5971.mailgun.org',
+    secratKey : 'CBS_Community'
 };
--- services/forgetPasswordService.js
+++ services/forgetPasswordService.js
@@ -16,7 +16,7 @@ exports.checkUserName = function(req,res){
             return 0;
         } else {
             var member = respBody.rows[0].doc;
-            var token = jwt.sign({ id: respBody.rows[0].doc._id }, 'CBS_Community',{
+            var token = jwt.sign({ id: respBody.rows[0].doc._id }, config.App.secratKey,{
                 expiresIn : 259200
             });
             console.log(token);
@@ -42,7 +42,7 @@ exports.checkUserName = function(req,res){
 
 exports.resetPassword = function(req,res){
     //console.log(req.body.userId);
-    var decoded = jwt.verify(req.body.userId, 'CBS_Community', function(err,decode){
+    var decoded = jwt.verify(req.body.userId, config.App.secratKey, function(err,decode){
         if(err){
             console.log(err);
 
