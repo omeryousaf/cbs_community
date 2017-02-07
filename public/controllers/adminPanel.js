4b6b37e458ce87eb7199b92f4d3040c50ea77cef
Shujaat Ali <m.ali.hussan.hussain@gmail.com>
2017-02-07 20:13:42 +0500

    started populating admin view

--- public/controllers/adminPanel.js
+++ public/controllers/adminPanel.js
@@ -1,4 +1,12 @@
 var controller = angular.module('adminPanel',[]);
-controller.controller('admin', ['ConfigService', '$scope', '$http', 'Upload', '$routeParams',function(ConfigService, $scope, $http, Upload, $routeParams){
-console.log("Burn");
+controller.controller('admin', ['ConfigService', '$scope', '$http', 'Upload', '$routeParams',
+    function(ConfigService, $scope, $http, Upload, $routeParams){
+        $scope.serverIp = ConfigService.serverIp;
+        $http.get( ConfigService.serverIp + '/members' ).success( function ( result ) {
+            $scope.members = result.members;
+            console.log(result.members[0]);
+        }).error(function (err) {
+            console.log('\nerror: ', err, '\n');
+            alert(err.error.message);
+        });
 }]);
