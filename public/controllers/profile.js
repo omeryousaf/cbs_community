0859f21110b66fac060cce41670a49ffb515672f
Omer Yousaf <u.yusaf@gmail.com>
2018-06-25 01:01:28 +0500

    removed code which should have been removed already but somehow got reinstated while resolving conflicts in last commit

--- public/controllers/profile.js
+++ public/controllers/profile.js
@@ -124,33 +124,6 @@ controller.controller('Profile', ['ConfigService', '$scope', '$http', 'Upload', 
                     });
                 };
                 reader.readAsDataURL( files[0]);
-                // listen to image transformation data ready event from the custom 'cropper' directive
-                $scope.$on('imgTransformDataReady', function(){
-                  var transformationData = arguments[1]; // arguments array contains data passed by event source (directive)
-                  var url = ConfigService.serverIp + '/upload-profile-image';
-                    return Upload.upload({
-                        url: url,
-                        fields: {
-                            x: transformationData.x,
-                            y: transformationData.y,
-                            width: transformationData.width,
-                            height: transformationData.height,
-                            scaleX: transformationData.scaleX,
-                            scaleY: transformationData.scaleY,
-                            rotate: transformationData.rotate,
-                            scaleToHeight: transformationData.scaleToHeight,
-                            scaleToWidth: transformationData.scaleToWidth,
-                            file: $scope.files[0]
-                        }
-                    }).success(function(response) {
-                        $scope.image = $scope.imageBackupPath = ConfigService.serverIp + response.filePath + '?' + new Date().valueOf();
-                        $scope.preview = ""; // is this var redundant now ? if yes, remove it from everywhere in this file
-                        destroyCropPanel($scope.editModal);
-                    }).error(function (err) {
-                        alert('error occurred while uploading image: ' + err);
-                        console.log( '\n', err, '\n');
-                    });
-                });
             }
         };
 
