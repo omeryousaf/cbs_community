5ac9f2bb6000162460192b8c2537d2507bc1b8f8
Omer Yousaf <u.yusaf@gmail.com>
2018-12-02 17:27:57 +0500

    added logout option and moved my profile and logout links inside the newly introduced 'Account' dropdown in nav bar

--- public/layout/navigation.component.js
+++ public/layout/navigation.component.js
@@ -1,3 +1,19 @@
+function navigationController(ConfigService, $http) {
+	this.selected = 'Account';
+
+	this.selectFromDropdown = function(newSelected) {
+		this.selected = newSelected;
+		if (newSelected === 'Logout') {
+			$http.get(ConfigService.serverIp + '/api/logout');
+		}
+	};
+
+	this.selectOther = function() {
+		this.selected = 'Account';
+	};
+}
+
 angular.module('navigationComponent', []).component('navigation', {
-  templateUrl: 'layout/navigation.template.html'
+  templateUrl: 'layout/navigation.template.html',
+  controller: ['ConfigService', '$http', navigationController]
 });
