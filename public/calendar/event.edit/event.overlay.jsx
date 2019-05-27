d9099253bd2058b1e58a313ef2227aa0afe2f40d
Omer Yousaf <u.yusaf@gmail.com>
2019-05-27 16:48:35 -0400

    added  action button and some styling to the event summary overlay

--- public/calendar/event.edit/event.overlay.jsx
+++ public/calendar/event.edit/event.overlay.jsx
@@ -4,44 +4,65 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+import styled from 'styled-components';
+import { spacing } from '@material-ui/system';
 import * as moment from 'moment';
 
+const StyledDiv = styled.div`${spacing}`;
 
 class EventOverlay extends React.Component {
+	constructor(props) {
+		super(props);
+		this.goToEventEditScreen = this.goToEventEditScreen.bind(this);
+	}
+
+	goToEventEditScreen() {
+		this.props.close();
+		window.location = '/calendar/event';
+	}
+
 	render() {
 		return(
 			<Dialog
 				open = {this.props.open}
 				onClose = {this.props.close}
 			>
-				<DialogTitle>{"Event Summary"}</DialogTitle>
+				<DialogTitle align='center'>
+					<span style={{ fontSize: '1.5em'}}>{'Event Summary'}</span>
+				</DialogTitle>
 				<DialogContent>
-					<div>
-						<span>Title: </span>
-						<DialogContentText display='inline'>
+					<StyledDiv mb={1}>
+						<DialogContentText display='inline' variant='h5'>Title: </DialogContentText>
+						<DialogContentText display='inline' color='textPrimary' variant='h5'>
 							{this.props.event.title}
 						</DialogContentText>
-					</div>
-					<div>
-						<span>Start: </span>
-						<DialogContentText display='inline'>
+					</StyledDiv>
+					<StyledDiv mb={1}>
+						<DialogContentText display='inline' variant='h5'>Start: </DialogContentText>
+						<DialogContentText display='inline' color='textPrimary' variant='h5'>
 							{moment(this.props.event.start).format('DD MMM YYYY @ hh:mm a')}
 						</DialogContentText>
-					</div>
-					<div>
-						<span>End: </span>
-						<DialogContentText display='inline'>
+					</StyledDiv>
+					<StyledDiv mb={1}>
+						<DialogContentText display='inline' variant='h5'>End: </DialogContentText>
+						<DialogContentText display='inline' color='textPrimary' variant='h5'>
 							{moment(this.props.event.end).format('DD MMM YYYY @ hh:mm a')}
 						</DialogContentText>
-					</div>
-					<div>
-						<span>Location: </span>
-						<DialogContentText display='inline'>
+					</StyledDiv>
+					<StyledDiv mb={1}>
+						<DialogContentText display='inline' variant='h5'>Location: </DialogContentText>
+						<DialogContentText display='inline' color='textPrimary' variant='h5'>
 							{this.props.event.location}
 						</DialogContentText>
-					</div>
-
+					</StyledDiv>
 				</DialogContent>
+				<DialogActions>
+					<Button onClick={this.goToEventEditScreen} color="primary" style={{ fontSize: '1em'}}>
+            Edit
+          </Button>
+				</DialogActions>
 			</Dialog>
 			);
 	}
